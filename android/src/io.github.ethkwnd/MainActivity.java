a0134da25c9ad6b404f2d0a3c4ebb37358acc42e
ganyuke <personalemailforedits@gmail.com>
2023-04-08 17:23:00 -0700

    add upload funcitonality

--- android/src/io.github.ethkwnd/MainActivity.java
+++ android/src/io.github.ethkwnd/MainActivity.java
@@ -17,6 +17,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import android.os.AsyncTask;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
@@ -82,8 +90,22 @@ private void stopRecording() {
         mediaRecorder = null;
         isRecording = false;
         Toast.makeText(this, "Recording stopped", Toast.LENGTH_SHORT).show();
-    }
+
+        // Show the upload button
+        Button uploadButton = findViewById(R.id.uploadButton);
+        uploadButton.setVisibility(View.VISIBLE);
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String wavFilePath = audioFilePath.replace(".3gp", ".wav");
+                convertToWav(audioFilePath, wavFilePath);
+                uploadFile(wavFilePath);
+            }
+    });
 }
+
+
 
 @Override
 public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -105,3 +127,70 @@ protected void onStop() {
     }
 }
 
+
+
+private void convertToWav(String inputPath, String outputPath) {
+    int returnCode = FFmpeg.execute("-i " + inputPath + " -acodec pcm_s16le -ar 44100 -ac 1 " + outputPath);
+
+    if (returnCode == RETURN_CODE_SUCCESS) {
+        Log.i("MainActivity", "Audio conversion to WAV successful");
+    } else {
+        Log.e("MainActivity", "Audio conversion to WAV failed");
+    }
+}
+
+private void uploadFile(String filePath) {
+    new AsyncTask<String, Void, Boolean>() {
+        @Override
+        protected Boolean doInBackground(String... params) {
+            String filePath = params[0];
+            String urlString = "https://example.org/upload";
+            HttpURLConnection connection = null;
+
+            try {
+                URL url = new URL(urlString);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+                connection.setUseCaches(false);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "audio/wav");
+                connection.connect();
+
+                InputStream inputStream = new BufferedInputStream(new FileInputStream(filePath));
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    connection.getOutputStream().write(buffer, 0, bytesRead);
+                }
+
+                int responseCode = connection.getResponseCode();
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    return true;
+                } else {
+                    return false;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+            if (result) {
+                Toast.makeText(MainActivity.this, "Upload successful", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Upload failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }.execute(filePath);
+}
